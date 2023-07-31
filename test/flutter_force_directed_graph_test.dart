8d468bc0dc3dc2fc7aa5ec799c8130f095b8103e
SkywalkerDarren <yh83533010yh2@gmail.com>
2023-07-31 23:02:46 +0800

    init

--- test/flutter_force_directed_graph_test.dart
+++ test/flutter_force_directed_graph_test.dart
@@ -3,10 +3,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_force_directed_graph/flutter_force_directed_graph.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+
 }
